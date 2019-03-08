d8fc5152993a05217a48ef1a1f583c71bf9ac93e
Tim Großmann <contact.timgrossmann@gmail.com>
2019-03-08 11:41:25 +0800

    refactor to class and use ES6 let/const

--- vortoMappingEngine.js
+++ vortoMappingEngine.js
@@ -1,82 +1,86 @@
 // Load dependencies
-var convertToXML = require("xml-js");
-var log = require("loglevel");
-var fontoxpath = require("fontoxpath"),
-    parser = require("slimdom-sax-parser");
+const convertToXML = require("xml-js");
+const log = require("loglevel");
+const fontoxpath = require("fontoxpath");
+const parser = require("slimdom-sax-parser");
 
-var mappingSpec = null;
 
-exports.setLogLevel = function (message) {
-    if (["trace", "debug", "info", "warn", "error"].includes(message)) {
-        log.setLevel(message);
+class VortoJsMapper {
+    constructor(mappingSpec) {
+        this.mappingSpec = mappingSpec;
     }
-}
+    
+    setMappingSpec = function (mappingSpec) {
+        this.mappingSpec = mappingSpec;
+    }
+    
+    setLogLevel = function (message) {
+        if (["trace", "debug", "info", "warn", "error"].includes(message)) {
+            log.setLevel(message);
+        }
+    }
+    
+    // avoid export of fontoxpath and just pass on all arguments to the fontoxpath method
+    registerCustomXPathFunction = function () {
+        fontoxpath.registerCustomXPathFunction.apply(null, arguments);
+    }
 
-exports.setMappingSpec = function (mapping) {
-    mappingSpec = mapping;
-}
+    transform = function (rawPayload) {
+        let outputObj = {};
 
-exports.fontoxpath = fontoxpath;
+        // Convert rawPayload to xml in order to use xpath
+        const options = { compact: true, ignoreComment: true };
+        let xmlRawPayload = convertToXML.json2xml(rawPayload, options);
 
-exports.transform = function (rawPayload) {
-    var outputObj = {};
+        // Add a root element 
+        xmlRawPayload = `<rootNode>${xmlRawPayload}</rootNode>`;
+        log.debug("Raw device payload in xml ...\n" + xmlRawPayload);
 
-    // Convert rawPayload to xml in order to use xpath
-    var options = { compact: true, ignoreComment: true };
-    var xmlRawPayload = convertToXML.json2xml(rawPayload, options);
+        const doc = parser.sync(xmlRawPayload);
 
-    // Add a root element 
-    xmlRawPayload = "<rootNode>" + xmlRawPayload + "</rootNode>";
-    log.debug("Raw device payload in xml ...\n" + xmlRawPayload);
+        // Iterate through the mapping spec and look for function blocks
+        // Step 1: Iterate over number of function blocks in the information model
 
-    const doc = parser.sync(xmlRawPayload);
+        try {
+            const numberOfFunctionBlocks = mappingSpec.infoModel.functionblocks.length;
+            if (numberOfFunctionBlocks) {
+                log.debug('Number of function blocks found = ' + numberOfFunctionBlocks);
+                for (let countFB = 0; countFB < numberOfFunctionBlocks; countFB++) {
+                    const fbName = mappingSpec.infoModel.functionblocks[countFB].name;
 
-    // Iterate through the mapping spec and look for function blocks
-    // Step 1: Iterate over number of function blocks in the information model
+                    let status = {};
 
-    try {
-        var numberOfFunctionBlocks = mappingSpec.infoModel.functionblocks.length;
-        if (numberOfFunctionBlocks) {
-            log.debug('Number of function blocks found = ' + numberOfFunctionBlocks);
-            for (var countFB = 0; countFB < numberOfFunctionBlocks; countFB++) {
-                var fbName = mappingSpec.infoModel.functionblocks[countFB].name;
+                    // Step 2: Search for status properties in the function block along with the mapping
+                    const numberOfStatusProperties = mappingSpec.properties[fbName].statusProperties.length;
+                    if (numberOfStatusProperties) {
+                        log.debug('Number of status properties found = ' + numberOfStatusProperties);
 
-                var status = {};
+                        for (let countSP = 0; countSP < numberOfStatusProperties; countSP++) {
+                            const statusPropertyName = mappingSpec.properties[fbName].statusProperties[countSP].name;
+                            const path = mappingSpec.properties[fbName].statusProperties[countSP].stereotypes[0].attributes.xpath;
 
-                // Step 2: Search for status properties in the function block along with the mapping
-                var numberOfStatusProperties = mappingSpec.properties[fbName].statusProperties.length;
-                if (numberOfStatusProperties) {
-                    log.debug('Number of status properties found = ' + numberOfStatusProperties);
+                            log.debug("path : " + path);
+                            if (path) {
 
-                    for (var countSP = 0; countSP < numberOfStatusProperties; countSP++) {
-                        var statusPropertyName = mappingSpec.properties[fbName].statusProperties[countSP].name;
-                        var path = mappingSpec.properties[fbName].statusProperties[countSP].stereotypes[0].attributes.xpath;
-
-                        log.debug("path : " + path);
-                        if (path) {
-
-                            // Step 3 : Evaluate xpath expression
-                            var xpathResult = fontoxpath.evaluateXPathToString(path, doc);
-                            log.debug("xpathResult = " + xpathResult);
-                            status[statusPropertyName] = xpathResult;
+                                // Step 3 : Evaluate xpath expression
+                                const xpathResult = fontoxpath.evaluateXPathToString(path, doc);
+                                log.debug("xpathResult = " + xpathResult);
+                                status[statusPropertyName] = xpathResult;
+                            }
                         }
                     }
-
+                    // Step 3: Add all properties under the user defined function block variable
+                    outputObj[fbName] = { "status": status };
                 }
-                // Step 3: Add all properties under the user defined function block variable
-                outputObj[fbName] = { "status": status };
-
             }
+        } catch (err) {
+            log.error("Error : " + err.message);
+            throw new Error('Failed!\n' + err.message);
         }
-
-
-    } catch (err) {
-        log.error("Error : " + err.message);
-        throw new Error('Failed!\n' + err.message);
+        
+        outputObjStr = JSON.stringify(outputObj, null, 0);
+        log.debug("Final output... \n" + outputObjStr);
+        
+        return outputObjStr;
     }
-
-    outputObj = JSON.stringify(outputObj, null, 0);
-    log.debug("Final output... \n" + outputObj);
-
-    return outputObj;
-};
+}
